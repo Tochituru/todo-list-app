537b4c47056b18ae3bb4441f70f79be14a13c3a4
Tochituru <rotrupo@gmail.com>
2019-12-05 10:17:15 -0300

    Delete tasks and persist state

--- src/App.js
+++ src/App.js
@@ -26,6 +26,24 @@ class App extends Component {
     this.setState({ todo: newTodo })
   }
 
+  deleteTask = (id) => {
+    let newTodo = [...this.state.todo]
+    let task = newTodo.find((e) => e.id === id)
+    newTodo.splice(newTodo.indexOf(task), 1)
+    this.setState({ todo: newTodo })
+  }
+
+  componentDidMount = () => {
+    const persistedState = window.localStorage.getItem('todo-state')
+    this.setState({ ...(JSON.parse(persistedState)) || { todo: ''} })
+
+  }
+
+  componentDidUpdate = () => {
+    let persisted = this.state.todo
+    window.localStorage.setItem('todo-state', JSON.stringify(persisted))
+  }
+
   render() {
     const complete = [...this.state.todo.filter((e) => e.status === 'complete')]
     const pending = [...this.state.todo.filter((e) => e.status === 'pending')]
@@ -42,8 +60,20 @@ class App extends Component {
           onKeyPress={(e) => this.enterHandler(e)}
         >
         </TextField>
-        <TaskList title={'Pendientes'} tag={'(___)'} data={pending} changeStatus={this.changeStatus} />
-        <TaskList title={'Completadas'} tag={'( X )'}data={complete} changeStatus={this.changeStatus} />
+        <TaskList
+          title={'Pendientes'}
+          tag={'(___)'}
+          data={pending}
+          changeStatus={this.changeStatus}
+          deleteTask={this.deleteTask}
+        />
+        <TaskList
+          title={'Completadas'}
+          tag={'( X )'}
+          data={complete}
+          changeStatus={this.changeStatus}
+          deleteTask={this.deleteTask}
+        />
       </Container>
     )
   }
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,16 +1,19 @@
 import React from 'react'
-import { List, ListItem, ListItemText } from '@material-ui/core'
+import { List, ListItem, ListItemText, Button } from '@material-ui/core'
 
 
-const TaskList = ({ data, changeStatus, tag, title }) => {
+const TaskList = ({ data, changeStatus, deleteTask, tag, title }) => {
     return (
         <List>
             <h2>{title}</h2>
             {data.map((e, i) => (
-                <ListItem button onClick={() => changeStatus(e.id)} key={i} >
-                    <ListItemText>
+                <ListItem button key={i} >
+                    <ListItemText onClick={() => changeStatus(e.id)}>
                         {`${tag} ${e.text} `}
                     </ListItemText>
+                    <Button variant={'contained'} onClick={() => deleteTask(e.id)}>
+                        Eliminar
+                    </Button>
                 </ListItem>))
             }
         </List>
